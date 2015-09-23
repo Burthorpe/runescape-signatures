2ba2b49932cfc4bad97f34ad682d8f615ff237b8
Wade Urry <wader@iwader.co.uk>
2015-09-23 22:57:27 +0100

    Rendering all the skills with calculated coordinates

--- src/Burthorpe/Runescape/Signature/Signature.php
+++ src/Burthorpe/Runescape/Signature/Signature.php
@@ -72,6 +72,11 @@ class Signature {
         return $this->getImage()->psrResponse('png', 90);
     }
 
+    protected function drawOverall()
+    {
+
+    }
+
     /**
      * Render the given skill onto the image
      *
@@ -86,8 +91,30 @@ class Signature {
         $icon = $this->getSkillIconFile($skill);
         $level = $this->getStats($skill)->get('level');
 
-        $image->insert($icon, 'top-left', 10, 10);
-        $image->text($level, 30, 22, $this->fontCallback());
+        $image->insert($icon, 'top-left', $x, ($y - 12)); // -12 to offset icon height
+        $image->text($level, ($x + 20), $y, $this->fontCallback()); // +20 to align text to the side of the icon
+    }
+
+    /**
+     * Get the x-axis coordinate to start drawing from depending on the skill given
+     *
+     * @param $skillId
+     * @return int
+     */
+    protected function getDrawLocationX($skillId)
+    {
+        return 10 + (floor(max($skillId - 1, 0) / 6) * 50);
+    }
+
+    /**
+     * Get the y-axis coordinate to start drawing from depending on the skill given
+     *
+     * @param $skillId
+     * @return int
+     */
+    protected function getDrawLocationY($skillId)
+    {
+        return 22 + (22 * (max($skillId - 1, 0) % 6));
     }
 
     /**
@@ -99,114 +126,21 @@ class Signature {
     {
         $this->api->getSkills()->each(function($skill)
         {
-            die($skill);
+            $id = $skill->get('id');
+
+            switch(mb_strtolower($skill->get('name')))
+            {
+                case 'overall';
+                    $this->drawOverall();
+                    break;
+                default:
+                    $this->drawSkill(
+                        $skill,
+                        $this->getDrawLocationX($id),
+                        $this->getDrawLocationY($id)
+                    );
+            }
         });
-
-        $this->drawSkill('attack');
-        $this->drawSkill('strength');
-
-        return;
-
-        $this->api->calculateCombatLevel(
-            $stats->get('attack')->get('level'),
-            $stats->get('strength')->get('level'),
-            $stats->get('magic')->get('level'),
-            $stats->get('ranged')->get('level'),
-            $stats->get('defence')->get('level'),
-            $stats->get('constitution')->get('level'),
-            $stats->get('prayer')->get('level'),
-            $stats->get('summoning')->get('level')
-        );
-
-        $image->insert($this->getSkillIconFile('attack'), 'top-left', 30, 10);
-        $image->text($stats->get('attack')->get('level'), 10, 22, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('strength'), 'top-left', 30, 31);
-        $image->text($stats->get('strength')->get('level'), 10, 44, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('defence'), 'top-left', 30, 53);
-        $image->text($stats->get('defence')->get('level'), 10, 66, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('constitution'), 'top-left', 30, 77);
-        $image->text($stats->get('constitution')->get('level'), 10, 89, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('ranged'), 'top-left', 30, 100);
-        $image->text($stats->get('ranged')->get('level'), 10, 111, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('prayer'), 'top-left', 32, 121);
-        $image->text($stats->get('prayer')->get('level'), 10, 134, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('magic'), 'top-left', 80, 10);
-        $image->text($stats->get('magic')->get('level'), 60, 22, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('summoning'), 'top-left', 80, 31);
-        $image->text($stats->get('summoning')->get('level'), 60, 44, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('cooking'), 'top-left', 80, 53);
-        $image->text($stats->get('cooking')->get('level'), 60, 66, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('fishing'), 'top-left', 80, 77);
-        $image->text($stats->get('fishing')->get('level'), 60, 89, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('firemaking'), 'top-left', 80, 100);
-        $image->text($stats->get('firemaking')->get('level'), 60, 111, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('crafting'), 'top-left', 80, 121);
-        $image->text($stats->get('crafting')->get('level'), 60, 134, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('woodcutting'), 'top-left', 130, 10);
-        $image->text($stats->get('woodcutting')->get('level'), 110, 22, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('fletching'), 'top-left', 130, 31);
-        $image->text($stats->get('fletching')->get('level'), 110, 44, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('smithing'), 'top-left', 130, 53);
-        $image->text($stats->get('smithing')->get('level'), 110, 66, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('mining'), 'top-left', 130, 77);
-        $image->text($stats->get('mining')->get('level'), 110, 89, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('herblore'), 'top-left', 130, 100);
-        $image->text($stats->get('herblore')->get('level'), 110, 111, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('agility'), 'top-left', 130, 121);
-        $image->text($stats->get('agility')->get('level'), 110, 134, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('thieving'), 'top-left', 180, 10);
-        $image->text($stats->get('thieving')->get('level'), 160, 22, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('runecrafting'), 'top-left', 180, 31);
-        $image->text($stats->get('runecrafting')->get('level'), 160, 44, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('slayer'), 'top-left', 180, 53);
-        $image->text($stats->get('slayer')->get('level'), 160, 66, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('farming'), 'top-left', 180, 77);
-        $image->text($stats->get('farming')->get('level'), 160, 89, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('construction'), 'top-left', 180, 100);
-        $image->text($stats->get('construction')->get('level'), 160, 111, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('hunter'), 'top-left', 180, 121);
-        $image->text($stats->get('hunter')->get('level'), 160, 134, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('dungeoneering'), 'top-left', 230, 10);
-        $image->text($stats->get('dungeoneering')->get('level'), $this->negateThreeDigits($stats->get('dungeoneering')->get('level'), 210), 22, $this->fontCallback());
-
-        $image->insert($this->getSkillIconFile('divination'), 'top-left', 230, 31);
-        $image->text($stats->get('divination')->get('level'), 210, 44, $this->fontCallback());
-
-
-    }
-
-    protected function negateThreeDigits($level, $xAxis)
-    {
-        if (strlen($level) === 3)
-        {
-            return $xAxis - 8;
-        }
-
-        return $xAxis;
     }
 
     /**
@@ -276,7 +210,7 @@ class Signature {
             );
         }
 
-        return (! is_null($skill) ? $stats->get($skill) : $stats);
+        return (! is_null($skill) ? $stats->get($skill->get('name')) : $stats);
     }
 
     /**
@@ -298,7 +232,7 @@ class Signature {
      */
     protected function getResourcesPath($path = '')
     {
-        return __DIR__ . '/Resources/' . $path;
+        return __DIR__ . '/Resources' . ($path ? DIRECTORY_SEPARATOR . $path : $path);
     }
 
     /**
@@ -308,7 +242,7 @@ class Signature {
      */
     protected function getFontFile()
     {
-        return $this->getResourcesPath('/Fonts/OpenSans/OpenSans-Bold.ttf');
+        return $this->getResourcesPath('Fonts/OpenSans/OpenSans-Bold.ttf');
     }
 
     /**
@@ -320,7 +254,7 @@ class Signature {
     protected function getSkillIconFile($skill)
     {
         return $this->getResourcesPath(
-            sprintf('/Images/Skills/%s.png', strtolower($skill))
+            sprintf('Images/Skills/%s.png', strtolower($skill->get('name')))
         );
     }
 
